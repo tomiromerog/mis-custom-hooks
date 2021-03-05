eceef7c435b0d2c6f7b57929c4c6087478574cde
tomiromerog <63374503+tomiromerog@users.noreply.github.com>
2021-03-05 18:09:29 -0300

    Primer Commit

---
+++ useFectch.js
@@ -0,0 +1,50 @@
+import {useEffect, useRef, useState} from 'react';
+
+export const useFectch = ( url ) => {
+
+    const isMounted = useRef(true);
+    
+
+    const [state, setstate] = useState({ data: null, loading: true, error: null})
+
+    useEffect( () => {
+
+        return () => {
+
+            isMounted.current = false;
+        }
+
+    }, [])
+
+    useEffect( () => {
+
+        setstate({data: null, loading: true, error: null })
+
+        fetch( url )
+            .then( resp => resp.json())
+            .then( data => {
+
+                if ( isMounted.current ){
+                    setstate({
+                        loading: false,
+                        error: null,
+                        data
+                    })
+                } else {
+                    console.log('set state no se mando')
+                }
+                
+            })
+            .catch( () => {
+                setstate({
+                    data: null,
+                    loadging: false,
+                    error: 'No se pudo cargar la info'
+                })
+            })
+
+    }, [url])
+
+    return state;
+
+}
